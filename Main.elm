725c3eea53399b0c2b4f4043d24f6f09b1ce5305
Lucas Vieira <vieiralucas4@gmail.com>
2017-03-22 00:08:37 -0300

    Cells can move

--- Main.elm
+++ Main.elm
@@ -2,31 +2,41 @@ module Main exposing (..)
 
 import Html exposing (Html)
 import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
 import Matrix exposing (Matrix)
+import Maybe.Extra
 
 
 -- MODEL
 
 
 type alias Cell =
-    Maybe Int
+    { x : Int
+    , y : Int
+    , v : Maybe Int
+    }
+
+
+type alias Board =
+    Matrix Cell
 
 
 type alias Model =
-    Matrix Cell
+    { board : Board
+    }
 
 
 initial : ( Model, Cmd Msg )
 initial =
     let
-        matrix =
+        board =
             Matrix.fromList
-                [ [ Just 8, Just 7, Just 6 ]
-                , [ Just 5, Just 4, Just 3 ]
-                , [ Just 2, Just 1, Nothing ]
+                [ [ Cell 0 0 (Just 8), Cell 1 0 (Just 7), Cell 2 0 (Just 6) ]
+                , [ Cell 0 1 (Just 5), Cell 1 1 (Just 4), Cell 2 1 (Just 3) ]
+                , [ Cell 0 2 (Just 2), Cell 1 2 (Just 1), Cell 2 2 Nothing ]
                 ]
     in
-        ( matrix, Cmd.none )
+        ( { board = board }, Cmd.none )
 
 
 
@@ -34,12 +44,54 @@ initial =
 
 
 type Msg
-    = NothignYet
+    = Move Cell
+
+
+neighbours : Cell -> Board -> List Cell
+neighbours cell board =
+    let
+        left =
+            Matrix.get (Matrix.loc cell.y (cell.x - 1)) board
+
+        right =
+            Matrix.get (Matrix.loc cell.y (cell.x + 1)) board
+
+        top =
+            Matrix.get (Matrix.loc (cell.y - 1) cell.x) board
+
+        bot =
+            Matrix.get (Matrix.loc (cell.y + 1) cell.x) board
+    in
+        Maybe.Extra.values [ top, bot, left, right ]
+
+
+move : Cell -> Board -> Board
+move cell board =
+    let
+        n =
+            neighbours cell board
+
+        isBlank c =
+            Maybe.Extra.isNothing c.v
+
+        blank =
+            List.filter isBlank n
+    in
+        case blank of
+            [] ->
+                board
+
+            blank :: _ ->
+                board
+                    |> Matrix.set (Matrix.loc blank.y blank.x) { blank | v = cell.v }
+                    |> Matrix.set (Matrix.loc cell.y cell.x) { cell | v = Nothing }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Move cell ->
+            ( { model | board = move cell model.board }, Cmd.none )
 
 
 
@@ -73,9 +125,13 @@ emptyStyle =
 
 viewCell : Cell -> Html Msg
 viewCell cell =
-    case cell of
+    case cell.v of
         Just v ->
-            Html.div [ style cellStyle ] [ (viewValue v) ]
+            Html.div
+                [ style cellStyle
+                , onClick (Move cell)
+                ]
+                [ (viewValue v) ]
 
         Nothing ->
             Html.div [ style emptyStyle ] []
@@ -102,7 +158,7 @@ view : Model -> Html Msg
 view model =
     let
         rows =
-            model
+            model.board
                 |> mapRows viewRow
                 |> List.map (Html.div [ style rowStyle ])
     in
